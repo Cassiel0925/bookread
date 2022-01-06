c707de75e319e1fd2c6a37f777149b79116e8fef
Cassiel0925 <1730604282@qq.com>
2022-01-06 14:28:03 +0800

    https://github.com/Cassiel0925/readbook.git

---
+++ src/views/ebook/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="ebook">
+        aaa
+    </div>
+</template>
+<script>
+export default {
+   name:'',
+}
+</script>
+<style lang="scss" rel="stylesheet/scss" scoped>
+@import "assets/styles/global";
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    configureWebpack: {
+        // resolve 解决路径相关的问题
+        resolve: {
+            //  文件的后缀名可以不写
+            // extensions: [],
+
+            // alias 别名
+            alias: {
+                // 默认的别名
+                // '@': 'src'
+
+                'assets': '@/assets',
+                'components': '@/components',
+                'views': '@/views',
+
+            }
+        }
+    }
+}
